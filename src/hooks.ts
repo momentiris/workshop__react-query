36a7f378e501fc8c932a8d1e7ba9be8860e5b156
Andreas Lundqvist <momentiris@gmail.com>
2021-10-08 09:19:44 +0200

    switch to normal axios

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,33 +1,80 @@
+import React from 'react';
 import axios, { AxiosError } from 'axios';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+
 import { AddPetRequest, Pet } from './types';
 
+type DataState<T> = {
+  data: T | undefined;
+  isLoading: boolean;
+  error: AxiosError | undefined;
+};
+
+const initialState = {
+  data: undefined,
+  isLoading: false,
+  error: undefined,
+};
+
 export const usePets = () => {
-  return useQuery<Pet[], AxiosError>(['pets'], () =>
-    axios.get<Pet[]>('/api/pets').then((res) => res.data)
-  );
+  const [state, setState] = React.useState<DataState<Pet[]>>(initialState);
+
+  React.useEffect(() => {
+    setState((current) => Object.assign({}, current, { loading: true }));
+    axios
+      .get<Pet[]>('/api/pets')
+      .then((res) =>
+        setState((current) =>
+          Object.assign({}, current, { loading: false, data: res.data })
+        )
+      );
+  }, []);
+
+  return { ...state, isFetching: state.isLoading };
 };
 
 export const usePetById = ({ id }: { id: string }) => {
-  return useQuery<Pet, AxiosError>(['pet', id], () =>
-    axios.get<Pet>(`/api/pet/${id}`).then((res) => res.data)
-  );
+  const [state, setState] = React.useState<DataState<Pet>>(initialState);
+
+  React.useEffect(() => {
+    setState((current) => Object.assign({}, current, { loading: true }));
+    axios
+      .get<Pet[]>(`/api/pet/${id}`)
+      .then((res) =>
+        setState((current) =>
+          Object.assign({}, current, { loading: false, data: res.data })
+        )
+      );
+  }, []);
+
+  return { ...state, isFetching: state.isLoading };
 };
 
 export const useCreatePet = () => {
-  const queryClient = useQueryClient();
+  const [state, setState] = React.useState<DataState<Pet>>(initialState);
+  const mutate = (pet: Pet) => {
+    setState((current) => Object.assign({}, current, { loading: true }));
+    return axios.post<AddPetRequest, Pet>('/api/pet', pet).then((res) => {
+      setState((current) =>
+        Object.assign({}, current, { loading: false, data: res })
+      );
+    });
+  };
 
-  return useMutation<Pet, AxiosError, AddPetRequest>(
-    (pet) => axios.post<AddPetRequest, Pet>('/api/pet', pet),
-    { onSuccess: () => queryClient.refetchQueries('pets') }
-  );
+  return { mutate, ...state, isFetching: state.isLoading };
 };
 
 export const useUpdatePet = () => {
-  const queryClient = useQueryClient();
+  const [state, setState] = React.useState<DataState<Pet>>(initialState);
+  const mutate = (pet: Pet) => {
+    setState((current) => Object.assign({}, current, { loading: true }));
+    return axios
+      .put<AddPetRequest, Pet>(`/api/pet/${pet.id}`, pet)
+      .then((res) => {
+        setState((current) =>
+          Object.assign({}, current, { loading: false, data: res })
+        );
+      });
+  };
 
-  return useMutation<Pet, AxiosError, Pet>(
-    (pet) => axios.put<Pet, Pet>(`/api/pet/${pet.id}`, pet),
-    { onSuccess: () => queryClient.refetchQueries('pet') }
-  );
+  return { mutate, ...state, isFetching: state.isLoading };
 };
